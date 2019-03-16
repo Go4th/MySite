50c6a0d16c572b4c750bf8631f883686c9064772
bgoforth <goforthbilly@gmail.com>
2019-03-15 21:38:58 -0500

    Another commit with code updates.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+//Components
+import { LandingPageComponent } from './landing-page/landing-page.component';
+
+const routes: Routes = [
+  {path: "", redirectTo: "landing", pathMatch: "full"},
+  {path: "landing", component:LandingPageComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
